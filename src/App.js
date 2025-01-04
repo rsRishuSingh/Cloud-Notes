e9db911b0d30b42e1b855573063eb8b0d1c16fae
Rishu <rk2787824@gmail.com>
2025-01-04 12:08:32 +0530

    router setu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar'
+import About from './components/About'
+import Home from './components/Home'
 function App() {
   return (
-   <h1>Hello World</h1>
+
+
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
